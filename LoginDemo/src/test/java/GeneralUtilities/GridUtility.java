29d4508c548d9bde52dac26451be4bf30643556a
vishnuprakashcs <108974955+vishnuprakashcs@users.noreply.github.com>
2022-07-12 00:38:13 +0530

    Add files via upload

---
+++ LoginDemo/src/test/java/GeneralUtilities/GridUtility.java
@@ -0,0 +1,18 @@
+package GeneralUtilities;
+
+import java.io.IOException;
+
+public class GridUtility
+{
+	public static void StartDockerGrid() throws IOException, InterruptedException
+	{
+		Runtime.getRuntime().exec("cmd /c start start_dockergrid.bat");
+		Thread.sleep(20000);
+	}
+	
+	public static void StopDockerGrid() throws IOException, InterruptedException
+	{
+		Runtime.getRuntime().exec("cmd /c start stop_dockergrid.bat");
+		Thread.sleep(20000);
+	}
+}
